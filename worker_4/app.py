5f7993cba0417071ebc9cb655d68f2fb382bb7f4
rohit20001221 <rohit20001221@gmail.com>
2021-05-27 23:34:57 +0530

    index scalp

---
+++ worker_4/app.py
@@ -0,0 +1,66 @@
+from kiteconnect.connect import KiteConnect
+from pymongo import MongoClient
+from functions_db import get_key_token
+from function_signals import *
+import datetime
+import os
+import threading
+import time
+
+os.environ['TZ'] = 'Asia/Kolkata'
+time.tzset()
+
+mongo = MongoClient('mongodb://db')
+db = mongo['intraday'+str(datetime.date.today())]
+collection = mongo['orders']
+
+mongo_clients = mongo_clients = MongoClient(
+    'mongodb+srv://jag:rtut12#$@cluster0.alwvk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+zerodha_id = os.environ['USERNAME']
+api_key, access_token = get_key_token(
+    zerodha_id, mongo_clients['client_details']['clients'])
+
+kite = KiteConnect(api_key=api_key, access_token=access_token)
+
+scalp_buy_investment = int(os.environ['SCALP_BUY_INVESTMENT'])
+scalp_sell_investment = int(os.environ['SCALP_SELL_INVESTMENT'])
+
+
+tickers_buy = []
+
+tickers_sell = []
+
+n = 900
+n_min = 15
+
+buy_quantity = int((scalp_buy_investment/len(tickers_buy))/3600/n_min)
+sell_quantity = int((scalp_sell_investment/len(tickers_sell))/3600/n_min)
+
+if buy_quantity < 1:
+    buy_quantity = 1
+
+if sell_quantity < 1:
+    sell_quantity = 1
+
+print('Worder 4 started')
+
+for ticker in tickers_buy:
+    t = threading.Thread(target=scalp_buy, args=[ticker, buy_quantity, n, kite])
+    t.start()
+
+for ticker in tickers_sell:
+    t = threading.Thread(target=scalp_sell, args=[ticker, sell_quantity, n, kite])
+    t.start()
+    
+import redis
+import json
+
+r = redis.StrictRedis(host='redis_pubsub', port=6379, decode_responses=True)
+
+while True:
+    positions = kite.positions()
+    data = json.dumps(positions)
+    
+    r.publish('positions', data)
+    time.sleep(n)
