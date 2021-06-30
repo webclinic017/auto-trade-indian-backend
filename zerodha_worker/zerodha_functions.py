76d55b9ebf5d01bb9588d10489ce055a0c7b6990
rohit20001221 <rohit20001221@gmail.com>
2021-06-30 11:24:29 +0530

    added tag to zerodha order api

--- zerodha_worker/zerodha_functions.py
+++ zerodha_worker/zerodha_functions.py
@@ -6,7 +6,7 @@ def retrieve_existing_positions(api_key, access_token):
     positions = kite.positions()
     return positions
 
-def market_buy_order(kite: KiteConnect, tradingsymbol, exchange, quantity):
+def market_buy_order(kite: KiteConnect, tradingsymbol, exchange, quantity, tag=None):
     order_id = kite.place_order(variety=kite.VARIETY_REGULAR, exchange=exchange,
                                 tradingsymbol=tradingsymbol,
                                 transaction_type=kite.TRANSACTION_TYPE_BUY, quantity=quantity,
@@ -14,10 +14,10 @@ def market_buy_order(kite: KiteConnect, tradingsymbol, exchange, quantity):
                                 price=None,
                                 validity=kite.VALIDITY_DAY,
                                 disclosed_quantity=None, trigger_price=None, squareoff=None, stoploss=None,
-                                trailing_stoploss=None, tag=None)
+                                trailing_stoploss=None, tag=tag)
     return order_id
 
-def market_sell_order(kite: KiteConnect, tradingsymbol, exchange, quantity):
+def market_sell_order(kite: KiteConnect, tradingsymbol, exchange, quantity, tag=None):
     order_id = kite.place_order(variety=kite.VARIETY_REGULAR, exchange=exchange,
                                 tradingsymbol=tradingsymbol,
                                 transaction_type=kite.TRANSACTION_TYPE_SELL, quantity=quantity,
@@ -25,25 +25,25 @@ def market_sell_order(kite: KiteConnect, tradingsymbol, exchange, quantity):
                                 price=None,
                                 validity=kite.VALIDITY_DAY,
                                 disclosed_quantity=None, trigger_price=None, squareoff=None, stoploss=None,
-                                trailing_stoploss=None, tag=None)
+                                trailing_stoploss=None, tag=tag)
     return order_id
 
-def limit_sell_order(kite: KiteConnect, tradingsymbol, exchange, quantity, price):
+def limit_sell_order(kite: KiteConnect, tradingsymbol, exchange, quantity, price, tag=None):
     order_id = kite.place_order(variety=kite.VARIETY_REGULAR, exchange=exchange,
                                 tradingsymbol=tradingsymbol,
                                 transaction_type=kite.TRANSACTION_TYPE_SELL, quantity=quantity,
                                 product=kite.PRODUCT_MIS, order_type=kite.ORDER_TYPE_LIMIT,
                                 price=price, validity=kite.VALIDITY_DAY,
                                 disclosed_quantity=None, trigger_price=None, squareoff=None, stoploss=None,
-                                trailing_stoploss=None, tag=None)
+                                trailing_stoploss=None, tag=tag)
     return order_id
 
-def limit_buy_order(kite: KiteConnect, tradingsymbol, exchange, quantity, price):
+def limit_buy_order(kite: KiteConnect, tradingsymbol, exchange, quantity, price, tag=None):
     order_id = kite.place_order(variety=kite.VARIETY_REGULAR, exchange=exchange,
                                 tradingsymbol=tradingsymbol,
                                 transaction_type=kite.TRANSACTION_TYPE_BUY, quantity=quantity,
                                 product=kite.PRODUCT_MIS, order_type=kite.ORDER_TYPE_LIMIT,
                                 price=price, validity=kite.VALIDITY_DAY,
                                 disclosed_quantity=None, trigger_price=None, squareoff=None, stoploss=None,
-                                trailing_stoploss=None, tag=None)
+                                trailing_stoploss=None, tag=tag)
     return order_id
