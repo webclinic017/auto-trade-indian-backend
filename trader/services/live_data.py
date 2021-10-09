461ba981d6e702099c98bbab3a9640fdcc2a93b4
rohit20001221 <rohit20001221@gmail.com>
2021-10-09 22:59:47 +0530

    live data kiteticker

--- trader/services/live_data.py
+++ trader/services/live_data.py
@@ -1,55 +1,94 @@
-import json, threading, os, requests, redis
-import time
-from flask import Flask
+from kiteconnect import KiteTicker
+import requests, os, json, redis
 
+# ZERODHA WORKER
 ZERODHA_SERVER = os.environ["ZERODHA_WORKER_HOST"]
 REDIS_SERVER = os.environ["REDIS_HOST"]
 
-r_ticker = redis.StrictRedis(host=REDIS_SERVER, port=6379)
-
-app = Flask(__name__)
-
-# root url
-@app.route("/")
-def home():
-    return "", 200
+# get the api_key and access_token
+credentials = requests.get(f"http://{ZERODHA_SERVER}/get_api_key_token").json()
+api_key, access_token = credentials["api_key"], credentials["access_token"]
 
 
-def get_liveData(tickers):
-    while True:
-        live_data = requests.post(
-            f"http://{ZERODHA_SERVER}/get/quote", json={"tickers": tickers}
-        ).json()
+# first make the mapping of ticker --> token
+token_map = requests.get(f"http://{ZERODHA_SERVER}/get/token_map").json()
 
-        for ticker in live_data:
-            tick = ticker
-            r_ticker.set(tick, json.dumps(live_data[ticker], default=str))
+# second make the mapping of token --> ticker
+ticker_map = {}
+db = json.loads(open("/app/data/tickers.json").read())
 
-        time.sleep(1)
+for ticker in db["tickers"]:
+    trading_symbol = ticker.split(":")[1]
+    try:
+        ticker_map[token_map[trading_symbol]["instrument_token"]] = ticker
+    except:
+        pass
+    ce_ticker = db["tickers"][ticker]["ce_ticker"].split(":")[1]
+    try:
+        ticker_map[token_map[ce_ticker]["instrument_token"]] = db["tickers"][ticker][
+            "ce_ticker"
+        ]
+    except:
+        pass
+    pe_ticker = db["tickers"][ticker]["pe_ticker"].split(":")[1]
+    try:
+        ticker_map[token_map[ce_ticker]["instrument_token"]] = db["tickers"][ticker][
+            "pe_ticker"
+        ]
+    except:
+        pass
+
+for ticker in db["index"]:
+    try:
+        ce_ticker = db["index"][ticker]["ce_ticker"].split(":")[1]
+        ticker_map[token_map[ce_ticker]["instrument_token"]] = db["index"][ticker][
+            "ce_ticker"
+        ]
+    except:
+        pass
+
+    try:
+        pe_ticker = db["index"][ticker]["pe_ticker"].split(":")[1]
+        ticker_map[token_map[pe_ticker]["instrument_token"]] = db["index"][ticker][
+            "pe_ticker"
+        ]
+    except:
+        pass
+
+
+tokens = list(ticker_map.keys())
+
+# kiteticker
+kws = KiteTicker(api_key=api_key, access_token=access_token)
+
+# redis connection
+rdb = redis.Redis(host=REDIS_SERVER)
+
+# callbacks on websockets
+def on_connect(ws, response):
+    ws.subscribe(tokens)
+    ws.set_mode(ws.MODE_FULL, tokens)
+
+
+def on_close(ws, code, reason):
+    print(reason)
+    ws.stop()
+
+
+def on_ticks(ws, ticks):
+
+    for tick in ticks:
+        print(tick)
+        ticker = ticker_map[tick["instrument_token"]]
+        rdb.set(ticker, json.dumps(tick, default=str))
+
+    print(len(ticks))
+
+
+kws.on_connect = on_connect
+kws.on_ticks = on_ticks
+kws.on_close = on_close
 
 
 def main():
-    data = json.loads(open("/app/data/tickers.json", "r").read())
-
-    stocks = list(data["tickers"].keys())
-
-    stocks_options = []
-    for stock in data["tickers"]:
-        stocks_options.append(data["tickers"][stock]["ce_ticker"])
-        stocks_options.append(data["tickers"][stock]["pe_ticker"])
-
-    index_options = []
-    for stock in data["index"]:
-        index_options.append(data["index"][stock]["ce_ticker"])
-        index_options.append(data["index"][stock]["pe_ticker"])
-
-    index = list(data["index"].keys())
-
-    tickers = stocks_options + index_options + stocks + index
-
-    t = threading.Thread(target=get_liveData, args=[tickers])
-    t.setDaemon(True)
-    t.start()
-
-    t = threading.Thread(target=app.run, args=["0.0.0.0", 8888])
-    t.start()
+    kws.connect(threaded=True)
