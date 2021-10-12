407f97dce6d527cb4b237a0a229a9132ff1b0bbb
rohit20001221 <rohit20001221@gmail.com>
2021-10-12 11:06:20 +0530

    bug fix

--- trader/services/stocks/worker_8.py
+++ trader/services/stocks/worker_8.py
@@ -97,8 +97,8 @@ class Worker8(TradeApp):
                         and self.tickers[ticker]["pe_ticker"]
                         not in self.entered_tickers
                         and self.price_diff(
-                            live_ce["depth"]["sell"][0]["price"],
-                            live_ce["depth"]["buy"][0]["price"],
+                            live_pe["depth"]["sell"][0]["price"],
+                            live_pe["depth"]["buy"][0]["price"],
                         )
                         < 5
                     ):
