7afac959c08b59de4dc77a550d8538cb871cba70
jonnalarohit <rohitjonnala@affordmed.com>
2021-11-29 11:07:01 +0530

    fix (URI): uri for user profile

--- trader/utils/auth.py
+++ trader/utils/auth.py
@@ -5,6 +5,6 @@ URI = "https://auto.bittrade.space/users/profile/detail"
 
 def get_key_token(zerodha_id):
     document = requests.get(
-        URI + zerodha_id, headers={"Authorization": f"Token {zerodha_id}"}
+        URI, headers={"Authorization": f"Token {zerodha_id}"}
     ).json()
     return document["api_key"], document["access_token"]
