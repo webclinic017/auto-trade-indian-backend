c2732189884b7a224a606be822a2b7d37297b856
rohit20001221 <rohit20001221@gmail.com>
2022-01-06 13:47:41 +0530

    ref (strategy): enabling bull bear

--- trader/services/live_data.py
+++ trader/services/live_data.py
@@ -41,8 +41,6 @@ def on_error(ws, code, reason):
 
 def appendTickers(ticks):
     for tick in ticks:
-        print(tick)
-
         ticker = ticker_map[tick["instrument_token"]]["tradingsymbol"]
         rdb.set(ticker, json.dumps(tick, default=str))
 
