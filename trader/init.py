7ecef9ed69f38aba15b5abd9c9b2dd913077dcba
rohit20001221 <rohit20001221@gmail.com>
2022-01-11 14:19:14 +0530

    init

--- trader/init.py
+++ trader/init.py
@@ -77,11 +77,11 @@ while True:
 # for the index trading
 services_index = [
     {"name": "bull_bear", "script": main_bull_bear, "args": []},
-    {
-        "name": "buyers_sellers",
-        "script": main_buyers_sellers,
-        "args": ["22", "1", "13"],
-    },
+    # {
+    #     "name": "buyers_sellers",
+    #     "script": main_buyers_sellers,
+    #     "args": ["22", "1", "13"],
+    # },
     # {"name": "costly_cheap", "script": main_costly_cheap, "args": ["22", "1", "13"]},
 ]
 
